3f60670e5b5e1dccc23d11b31334661c58b7e67a
ckopec-dev <c_kopec@hotmail.com>
2025-10-14 17:45:59 -0400

    Main build

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        const string mutexName = "Global\\MyUniqueDotNet8AppMutex";
+
+        using var mutex = new Mutex(initiallyOwned: true, name: mutexName, out bool createdNew);
+
+        if (!createdNew)
+        {
+            Console.WriteLine("Another instance of this program is already running.");
+            return;
+        }
+
+        Console.WriteLine("Program started. Press Enter to exit...");
+        Console.ReadLine();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Single-Instance
+
+- Example of a console application that prevents another instance of the program from running on the same machine.
